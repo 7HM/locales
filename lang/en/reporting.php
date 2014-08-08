5d1bccd50a002c28be8f49427131f6019ca3cf85
Dave Tosh <davidgtosh@gmail.com>
2014-08-08 12:43:46 -0400

    en added

---
+++ lang/en/reporting.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+|----------------------------------------------------------------------------
+| Translations for reporting
+|----------------------------------------------------------------------------
+*/
+
+return array(
+    'create'  => 'Create report',
+    'view'    => 'View report',
+    'deleted' => 'That report has been deleted',
+    'who'     => 'Who',
+    'did'     => 'Did',
+    'what'    => 'What',
+    'where'   => 'Where',
+    'with_result' => 'With result',
+    'dates'   => 'Dates',
+    'related' => 'Related statements',
+    'clear'   => 'Clear',
+    'save'    => 'Save',
+    'query'   => 'Query',
+    'run_query' => 'Run query',
+    'name'    => 'Name',
+    'created' => 'Created',
+    'desc'    => 'Description',
+    'run'     => 'Run',
+    'view_stats' => 'View statements'
+);
